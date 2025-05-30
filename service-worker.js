5f782261cfa3c53cbf809a4bfa172003a6d40921
bonich79 <boyprav@gmail.com>
2025-05-30 14:42:56 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,19 @@
 self.addEventListener('install', function(event) {
-  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open('sticker-cache-v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon.png'
+      ]);
+    })
+  );
 });
-
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
